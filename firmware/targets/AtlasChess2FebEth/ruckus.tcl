c381d2a87a28f2d1006a6009d562bc41741d8f4d
Larry Ruckman <ruckman@slac.stanford.edu>
2016-11-17 21:37:12 -0800

    Updating the submodules and Makefiles

--- firmware/targets/AtlasChess2FebEth/ruckus.tcl
+++ firmware/targets/AtlasChess2FebEth/ruckus.tcl
@@ -1,9 +1,13 @@
 # Load RUCKUS environment and library
 source -quiet $::env(RUCKUS_DIR)/vivado_proc.tcl
 
-# Load Source Code
+# Load common and sub-module ruckus.tcl files
+loadRuckusTcl $::env(PROJ_DIR)/../../submodules/surf
+loadRuckusTcl $::env(PROJ_DIR)/../../common/AtlasChess2Feb
+
+# Load local source Code
+loadSource -dir  "$::DIR_PATH/hdl/"
 loadSource -path "$::DIR_PATH/Version.vhd"
-loadSource -path "$::DIR_PATH/hdl/AtlasChess2FebEth.vhd"
 
 # Load Constraints
 loadConstraints -path "$::DIR_PATH/../../common/AtlasChess2Feb/xdc/AtlasChess2Feb.xdc"
