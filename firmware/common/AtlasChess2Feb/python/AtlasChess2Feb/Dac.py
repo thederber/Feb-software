d43e2a4664c1912f44c17a177cecd16e223ac798
Larry Ruckman <ruckman@slac.stanford.edu>
2016-12-04 12:38:19 -0800

    backing up progress

--- firmware/common/AtlasChess2Feb/python/AtlasChess2Feb/Dac.py
+++ firmware/common/AtlasChess2Feb/python/AtlasChess2Feb/Dac.py
@@ -29,34 +29,34 @@ def create(name='Dac', offset=0, memBase=None, hidden=False):
 
     dev.add(pyrogue.Variable(name='dacCASC',
                              description='DAC CASC',
-                             hidden=False, enum=None, offset=0x00000, bitSize=16, bitOffset=0, base='uint', mode='RW'))
+                             hidden=False, enum=None, offset=0x00000, bitSize=16, bitOffset=0, base='hex', mode='RW'))
 
     dev.add(pyrogue.Variable(name='dacPIXTH',
                              description='DAC PIXTH',
-                             hidden=False, enum=None, offset=0x00004, bitSize=16, bitOffset=0, base='uint', mode='RW'))   
+                             hidden=False, enum=None, offset=0x00004, bitSize=16, bitOffset=0, base='hex', mode='RW'))   
 
     dev.add(pyrogue.Variable(name='dacBLR',
                              description='DAC BLR',
-                             hidden=False, enum=None, offset=0x00008, bitSize=16, bitOffset=0, base='uint', mode='RW')) 
+                             hidden=False, enum=None, offset=0x00008, bitSize=16, bitOffset=0, base='hex', mode='RW')) 
 
     dev.add(pyrogue.Variable(name='dacBL',
                              description='DAC BL',
-                             hidden=False, enum=None, offset=0x0000C, bitSize=16, bitOffset=0, base='uint', mode='RW'))     
+                             hidden=False, enum=None, offset=0x0000C, bitSize=16, bitOffset=0, base='hex', mode='RW'))     
 
     dev.add(pyrogue.Variable(name='dacLvdsVCOM',
                              description='DAC LVDS_VCOM',
-                             hidden=False, enum=None, offset=0x10000, bitSize=16, bitOffset=0, base='uint', mode='RW'))
+                             hidden=False, enum=None, offset=0x10000, bitSize=16, bitOffset=0, base='hex', mode='RW'))
 
     dev.add(pyrogue.Variable(name='dacLvdsVctrl',
                              description='DAC LVDS_VCTRL',
-                             hidden=False, enum=None, offset=0x10004, bitSize=16, bitOffset=0, base='uint', mode='RW'))   
+                             hidden=False, enum=None, offset=0x10004, bitSize=16, bitOffset=0, base='hex', mode='RW'))   
 
     dev.add(pyrogue.Variable(name='dacRefP',
                              description='DAC REFP',
-                             hidden=False, enum=None, offset=0x10008, bitSize=16, bitOffset=0, base='uint', mode='RW'))
+                             hidden=False, enum=None, offset=0x10008, bitSize=16, bitOffset=0, base='hex', mode='RW'))
 
     dev.add(pyrogue.Variable(name='dacRefN',
                              description='DAC REFN',
-                             hidden=False, enum=None, offset=0x1000C, bitSize=16, bitOffset=0, base='uint', mode='RW'))                                 
+                             hidden=False, enum=None, offset=0x1000C, bitSize=16, bitOffset=0, base='hex', mode='RW'))                                 
 
     return dev
--- firmware/common/AtlasChess2Feb/python/AtlasChess2Feb/SysReg.py
+++ firmware/common/AtlasChess2Feb/python/AtlasChess2Feb/SysReg.py
@@ -29,11 +29,11 @@ def create(name='SysReg', offset=0, memBase=None, hidden=False):
 
     dev.add(pyrogue.Variable(name='refLockedCnt',
                              description='Reference clock Locked Status Counter',
-                             hidden=False, enum=None, offset=0x000, bitSize=32, bitOffset=0, base='uint', mode='RO'))                                         
+                             hidden=False, enum=None, offset=0x000, bitSize=32, bitOffset=0, base='hex', mode='RO'))                                         
                                          
     dev.add(pyrogue.Variable(name='refLocked',
                              description='Reference clock Locked Status',
-                             hidden=False, enum=None, offset=0x100, bitSize=1, bitOffset=0, base='uint', mode='RO'))
+                             hidden=False, enum=None, offset=0x100, bitSize=1, bitOffset=0, base='bool', mode='RO'))
                              
     dev.add(pyrogue.Variable(name='refClkFreq',
                              description='Reference clock frequency (units of Hz)',
@@ -41,54 +41,56 @@ def create(name='SysReg', offset=0, memBase=None, hidden=False):
                              
     dev.add(pyrogue.Variable(name='refSelect',
                              description='0x0 = local 40 MHz OSC, 0x1 = external 40 MHz reference',
-                             hidden=False, enum=None, offset=0x800, bitSize=1, bitOffset=0, base='uint', mode='RW'))
+                             hidden=False, enum=None, offset=0x800, bitSize=1, bitOffset=0, base='bool', mode='RW'))
                              
     dev.add(pyrogue.Variable(name='timingMode',
                              description='0x0 = LEMO Triggering, 0x1 = PGP Triggering, 0x2 = EVR Triggering',
-                             hidden=False, enum=None, offset=0x804, bitSize=2, bitOffset=0, base='uint', mode='RW'))
+                             hidden=False, 
+                             enum = {0:'TIMING_LEMO_TRIG_C',1:'TIMING_PGP_TRIG_C',2:'TIMING_SLACE_EVR_C',3:'TIMING_RESERVED'},
+                             offset=0x804, bitSize=2, bitOffset=0, base='enum', mode='RW'))
                              
     dev.add(pyrogue.Variable(name='pllRst',
                              description='PLL reset',
-                             hidden=False, enum=None, offset=0x808, bitSize=1, bitOffset=0, base='uint', mode='WO'))
+                             hidden=False, enum=None, offset=0x808, bitSize=1, bitOffset=0, base='bool', mode='WO'))
                              
     dev.add(pyrogue.Variable(name='dlyRst',
                              description='Delay FIFOs reset',
-                             hidden=False, enum=None, offset=0x80C, bitSize=1, bitOffset=0, base='uint', mode='WO'))
+                             hidden=False, enum=None, offset=0x80C, bitSize=1, bitOffset=0, base='bool', mode='WO'))
                              
     dev.add(pyrogue.Variable(name='dlyTiming',
                              description='ASIC timingpath delay FIFO configuration (units of 1/320MHz)',
-                             hidden=False, enum=None, offset=0x810, bitSize=12, bitOffset=0, base='uint', mode='RW'))
+                             hidden=False, enum=None, offset=0x810, bitSize=12, bitOffset=0, base='hex', mode='RW'))
                              
     dev.add(pyrogue.Variable(name='dlyChess',
                              description='ASIC datapath delay FIFO configuration (units of 1/320MHz)',
-                             hidden=False, enum=None, offset=0x814, bitSize=12, bitOffset=0, base='uint', mode='RW'))                             
+                             hidden=False, enum=None, offset=0x814, bitSize=12, bitOffset=0, base='hex', mode='RW'))                             
                              
     dev.add(pyrogue.Variable(name='destId',
                              description='ASIC packet header DEST ID',
-                             hidden=False, enum=None, offset=0x818, bitSize=6, bitOffset=0, base='uint', mode='RW'))
+                             hidden=False, enum=None, offset=0x818, bitSize=6, bitOffset=0, base='hex', mode='RW'))
                              
     dev.add(pyrogue.Variable(name='frameType',
                              description='ASIC packet header frame type',
-                             hidden=False, enum=None, offset=0x81C, bitSize=32, bitOffset=0, base='uint', mode='RW'))
+                             hidden=False, enum=None, offset=0x81C, bitSize=32, bitOffset=0, base='hex', mode='RW'))
 
     dev.add(pyrogue.Variable(name='pktWordSize',
                              description='ASIC Packet Size (in units of 16-bits words)',
-                             hidden=False, enum=None, offset=0x820, bitSize=8, bitOffset=0, base='uint', mode='RW'))                             
+                             hidden=False, enum=None, offset=0x820, bitSize=8, bitOffset=0, base='hex', mode='RW'))                             
                                                     
     dev.add(pyrogue.Variable(name='rollOverEn',
                              description='RollOverEn',
-                             hidden=False, enum=None, offset=0xf00, bitSize=1, bitOffset=0, base='uint', mode='RW'))
+                             hidden=False, enum=None, offset=0xf00, bitSize=1, bitOffset=0, base='bool', mode='RW'))
 
     dev.add(pyrogue.Variable(name='counterReset',
                              description='CounterReset',
-                             hidden=False, enum=None, offset=0xff4, bitSize=1, bitOffset=0, base='uint', mode='WO'))
+                             hidden=False, enum=None, offset=0xff4, bitSize=1, bitOffset=0, base='bool', mode='WO'))
 
     dev.add(pyrogue.Variable(name='softReset',
                              description='SoftReset',
-                             hidden=False, enum=None, offset=0xff8, bitSize=1, bitOffset=0, base='uint', mode='WO'))
+                             hidden=False, enum=None, offset=0xff8, bitSize=1, bitOffset=0, base='bool', mode='WO'))
 
     dev.add(pyrogue.Variable(name='hardReset',
                              description='HardReset',
-                             hidden=False, enum=None, offset=0xffc, bitSize=1, bitOffset=0, base='uint', mode='WO'))                                                               
+                             hidden=False, enum=None, offset=0xffc, bitSize=1, bitOffset=0, base='bool', mode='WO'))                                                               
 
     return dev
