9738cca8b40214af2a2a1b31c23ae16bdaa95bae
Larry Ruckman <ruckman@slac.stanford.edu>
2017-06-22 09:21:59 -0700

    adding Vivado version checking

--- firmware/common/AtlasChess2Feb/ruckus.tcl
+++ firmware/common/AtlasChess2Feb/ruckus.tcl
@@ -1,5 +1,11 @@
 # Load RUCKUS library
 source -quiet $::env(RUCKUS_DIR)/vivado_proc.tcl
 
+## Check for version 2016.4 of Vivado
+if { [VersionCheck 2016.4] < 0 } {
+   close_project
+   exit -1
+}
+
 # Load Source Code
 loadSource -dir "$::DIR_PATH/rtl/"
